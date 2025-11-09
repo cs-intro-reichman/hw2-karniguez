46b6f6bccc9860763f1884cf2ff3156b3d466c07
Karni Guez <karni.guez@post.runi.ac.il>
2025-11-09 16:07:23 +0200

    Add file

--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,45 @@
 public class TimeCalc {
     public static void main(String[] args) {
         // Replace this comment with your code
-    }
-}
+        int hours = Integer.parseInt("" + args[0].charAt( 0) + args[0].charAt(1));
+		// Does the same with the minutes part of the input.
+		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
+		String sMin;
+		String sHow;
+		
+	
+		int add = Integer.parseInt((args [1]));
+		int hou = (add / 60 );
+		int min = (add % 60);
+
+		int addmin = (minutes + min);
+		int neWamin = (addmin / 60);
+		int neWmin = (addmin % 60);
+		    if (neWmin < 10){
+				sMin = ("0" + neWmin);
+			}
+			else{
+			    sMin = ("" + neWmin);
+			}
+	
+		int neWhou = (hours + hou + neWamin);
+			if  (neWhou > 23){
+				sHow = ( "" + (neWhou % 24));
+			}
+			else if ((neWhou % 24) < 10){
+				sHow = ("0" + (neWhou % 24));
+			}
+			else if (neWhou < 10){
+				sHow = ("0" + neWhou);
+			}
+			else {
+				sHow = ("" + neWhou);
+			}
+		System.out.println(sHow + ":" + sMin);
+		}
+	}
+
+		
+		
+
+    
