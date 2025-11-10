40d53ba249d57f47a3360449ba5d47a663f69b36
Karni Guez <karni.guez@post.runi.ac.il>
2025-11-10 18:34:02 +0200

    add file

--- Cheers.java
+++ Cheers.java
@@ -1,3 +1,6 @@
+
+import com.sun.source.tree.WhileLoopTree;
+
 //feedback
 
 /*
@@ -9,5 +12,31 @@ Great work boaz!
 public class Cheers {
         public static void main(String[] args) {
 	    // Replace this comment with your code
+            String word = args [0];
+            int number = Integer.parseInt(args[1]);
+            String vowels = "AEFHILMSXNOR";
+            int i = 0;
+            while (i < word.length()){
+                char c = (word.charAt(i));
+                if (vowels.indexOf(c) != -1){
+                System.out.println("Give me " + "an " + c + ": " + c + "!");
+                }else{
+                    System.out.println("Give me " + "a  " + c + ": " + c + "!");
+                }
+                i++;
+                } 
+                System.out.println("What does that spell?");
+            int j = 0;
+            while (j < number) {
+                System.out.println(word + "!!!"); 
+                j++;
         }
-}
+              
+        
+            }
+
+        }
+
+
+
+
