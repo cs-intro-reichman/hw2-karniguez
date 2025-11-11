28996212d000b27a45d451169c6f3aaafbe49bc3
Karni Guez <karni.guez@post.runi.ac.il>
2025-11-11 20:37:06 +0200

    add file Calc

--- CalcPi.java
+++ CalcPi.java
@@ -2,5 +2,20 @@
 public class CalcPi {
 	public static void main(String [] args) { 
 	    // Replace this comment with your code
+		int number = Integer.parseInt(args[0]);
+		double co = 0;
+		//int app1 = 0;
+		//int app2 = 0;
+		for (int i = 1; i <= number; i++){
+			if (i % 2 == 0){
+				co -= 1.0/((i*2) - 1);
+			}
+			else {
+				co += 1.0/(i*2 - 1);
+			}
+		}
+		double tot = co * 4;
+		System.out.println("pi according to Java: " + Math.PI);
+		System.out.println("pi, approximated:     " + tot);
+		}
 	}
-}
